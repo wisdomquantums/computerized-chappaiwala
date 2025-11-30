1ca20620503e6318518b85fb7a796a86f9a29250
wisdomquantums <info@wisdomquantums.com>
2025-11-30 18:15:07 +0530

    fixes

--- frontend/src/layouts/UserLayout.jsx
+++ frontend/src/layouts/UserLayout.jsx
@@ -4,9 +4,9 @@ import Navbar from "../components/common/Navbar";
 
 const UserLayout = () => {
   return (
-    <div className="flex min-h-screen flex-col bg-slate-50">
+    <div className="flex min-h-screen flex-col bg-slate-50 overflow-x-hidden">
       <Navbar />
-      <main className="flex-1 bg-gradient-to-b from-white via-white to-slate-50">
+      <main className="flex-1 bg-gradient-to-b from-white via-white to-slate-50 overflow-x-hidden">
         <Outlet />
       </main>
       <Footer />
